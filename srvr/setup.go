ada259db705889389d29d8344b093926a4e3624d
Bruce Ediger <bediger8@gmail.com>
2022-04-20 08:28:11 -0600

    Setup() method beginnings

---
+++ srvr/setup.go
@@ -0,0 +1,20 @@
+package srvr
+
+import (
+	"os"
+)
+
+func (s *Srvr) Setup() {
+	s.LogDescriptor = os.Stderr
+	if s.Logfile != "" {
+		if fd, err := os.OpenFile(s.Logfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644); err != nil {
+			s.Infof("problem opening log file %q: %v", s.Logfile, err)
+			s.Infof("logging to stderr")
+		} else {
+			s.LogDescriptor = fd
+			s.Debugf("logging to file %q", s.Logfile)
+		}
+	} else {
+		s.Logfile = "stderr"
+	}
+}
