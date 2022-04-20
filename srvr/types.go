76f7e8b06e7e95d17febafb9b29deea260863342
Bruce Ediger <bediger8@gmail.com>
2022-04-20 08:26:58 -0600

    differentiate between "data" and "log" output

--- srvr/types.go
+++ srvr/types.go
@@ -2,11 +2,16 @@ package srvr
 
 import (
 	"net/http"
+	"os"
 )
 
 // Srvr holds all info that's used across instances
 // of this web application.
 type Srvr struct {
-	Router *http.ServeMux
-	Debug  bool
+	Router         *http.ServeMux
+	Debug          bool
+	Logfile        string
+	LogDescriptor  *os.File
+	Datafile       string
+	DataDescriptor *os.File
 }
