7ffa4058a0398b85f4e1a63e4c0d96b50cd4c68f
Bruce Ediger <bediger8@gmail.com>
2022-04-20 17:48:45 -0600

    move data output types to types.go

--- srvr/types.go
+++ srvr/types.go
@@ -3,6 +3,7 @@ package srvr
 import (
 	"net/http"
 	"os"
+	"time"
 )
 
 // Srvr holds all info that's used across instances
@@ -16,4 +17,26 @@ type Srvr struct {
 	LogDescriptor  *os.File
 	Datafile       string
 	DataDescriptor *os.File
+}
+
+// NameValuePair carries HTTP header names and values
+type NameValuePair struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// LogEntry holds HTTP request data in preparation for
+// JSON marshalling
+type LogEntry struct {
+	ReceptionTime time.Time        `json:"recpt_time"`
+	Method        string           `json:"method"`
+	URL           string           `json:"url"`
+	UserAgent     string           `json:"user_agent"`
+	RequestURI    string           `json:"request_uri"`
+	Protocol      string           `json:"proto"`
+	ContentLength int64            `json:"content_len"`
+	Host          string           `json:"host_addr"`
+	Remote        string           `json:"remote_addr"`
+	Headers       []*NameValuePair `json:"headers"`
+	Encoding      []string         `json:"transfer_encoding,omitempty"`
 }
