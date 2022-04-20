d2ec4cd5907ec84297b54f86e7c685c28c7a02a1
Bruce Ediger <bediger8@gmail.com>
2022-04-20 07:45:54 -0600

    add log file and debug file cmd line flags

--- main.go
+++ main.go
@@ -11,11 +11,15 @@ import (
 func main() {
 	portString := flag.String("p", "8080", "TCP port on which to listen")
 	debug := flag.Bool("v", false, "verbose output per request")
+	logFile := flag.String("o", "websrvr.log", "log file")
+	dataFile := flag.String("d", "websrvr.data", "JSON data file")
 	flag.Parse()
 
 	srv := &srvr.Srvr{
-		Router: http.NewServeMux(),
-		Debug:  *debug,
+		Router:   http.NewServeMux(),
+		Debug:    *debug,
+		Logfile:  *logFile,
+		Datafile: *dataFile,
 	}
 
 	srv.Routes()
