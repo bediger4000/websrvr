a0b4b127500d743f1a68ab7b77683823e8102346
Bruce Ediger <bediger8@gmail.com>
2022-04-25 10:41:27 -0600

    work on web server receiving files from "browsers"
    Why is this so byzantine?

--- srvr/handlers.go
+++ srvr/handlers.go
@@ -3,6 +3,7 @@ package srvr
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -34,9 +35,7 @@ func (s *Srvr) handleSlash() http.HandlerFunc {
 			info.Encoding = r.TransferEncoding
 		}
 
-		hdr := r.Header
-
-		for key, values := range hdr {
+		for key, values := range r.Header {
 			for _, value := range values {
 				nvp := &NameValuePair{
 					Name:  key,
@@ -46,43 +45,90 @@ func (s *Srvr) handleSlash() http.HandlerFunc {
 			}
 		}
 
-		if err := r.ParseForm(); err == nil {
-			if len(r.Form) > 0 {
-				for key, values := range r.Form {
-					for _, value := range values {
-						nvp := &NameValuePair{
-							Name:  key,
-							Value: value,
-						}
-						info.Form = append(info.Form, nvp)
-					}
-				}
+		for _, c := range r.Cookies() {
+			ce := &CookieEntry{
+				Name:       c.Name,
+				Value:      c.Value,
+				Path:       c.Path,
+				Expires:    c.Expires,
+				RawExpires: c.RawExpires,
+				MaxAge:     c.MaxAge,
+				Secure:     c.Secure,
+				HttpOnly:   c.HttpOnly,
+				SameSite:   int(c.SameSite),
+				Raw:        c.Raw,
+				Unparsed:   c.Unparsed,
 			}
-		} else {
-			s.Infof("http.Request.ParseForm(): %v", err)
+			info.Cookies = append(info.Cookies, ce)
 		}
 
-		contentTypes := r.Form["Content-Type"]
+		multiPart := false
+		contentTypes := r.Header["Content-Type"]
 		for _, ct := range contentTypes {
-			if ct == "multipart/form-data" {
-				if err := r.ParseMultipartForm(10 * 1024 * 1024); err == nil {
-					fmt.Printf("multi-part form %T, %v\n", r.MultipartForm, r.MultipartForm)
-					if r.MultipartForm != nil {
-						fmt.Printf("multi-part form\n")
-						for key, value := range r.MultipartForm.Value {
-							fmt.Printf("%q: %q\n", key, value)
-						}
-						/*
-							type Form struct {
-								Value map[string][]string
-								File  map[string][]*FileHeader
+			if strings.HasPrefix(ct, "multipart/form-data") {
+				fmt.Printf("multipart/form-data 0\n")
+				multiPart = true
+				break
+			}
+		}
+
+		if multiPart {
+			fmt.Printf("multipart/form-data 1\n")
+			if err := r.ParseMultipartForm(10 * 1024 * 1024); err == nil {
+				fmt.Printf("multipart/form-data 2\n")
+				if r.MultipartForm != nil {
+					fmt.Printf("multipart/form-data 3\n")
+					for key, values := range r.MultipartForm.Value {
+						for _, value := range values {
+							nvp := &NameValuePair{
+								Name:  key,
+								Value: value,
 							}
-						*/
-					} else {
+							info.Form = append(info.Form, nvp)
+						}
 					}
+					for field, fileheaders := range r.MultipartForm.File {
+						for _, value := range fileheaders {
+							fmt.Printf("field %q\n", field)
+							fmt.Printf("\tfilename %q\n", value.Filename)
+						}
+					}
+					/*
+						type FileHeader struct {
+							Filename string
+							Header   textproto.MIMEHeader
+							Size     int64
+
+							content []byte
+							tmpfile string
+						}
+
+						type Form struct {
+							Value map[string][]string
+							File  map[string][]*FileHeader
+						}
+					*/
 				} else {
-					s.Infof("http.Request.MultipartParseForm(): %v", err)
+					fmt.Printf("nil multi-part form\n")
 				}
+			} else {
+				s.Infof("http.Request.MultipartParseForm(): %v", err)
+			}
+		} else {
+			if err := r.ParseForm(); err == nil {
+				if len(r.Form) > 0 {
+					for key, values := range r.Form {
+						for _, value := range values {
+							nvp := &NameValuePair{
+								Name:  key,
+								Value: value,
+							}
+							info.Form = append(info.Form, nvp)
+						}
+					}
+				}
+			} else {
+				s.Infof("http.Request.ParseForm(): %v", err)
 			}
 		}
 
