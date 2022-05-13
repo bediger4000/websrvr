e84d4b1895f1804f603487cdea2c59dc0022f96e
Bruce Ediger <bediger8@gmail.com>
2022-05-12 18:05:35 -0600

    handle robots.txt requests

---
+++ srvr/robots.go
@@ -0,0 +1,33 @@
+package srvr
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var robotsTxt = `User-agent: Googlebot
+Disallow: /wp/
+
+User-agent: *
+Allow: /
+
+User-agent: *
+Disallow: /porn
+
+User-agent: *
+Disallow: /flapjackattack
+`
+
+func handleRobotsTxt(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Last-Modified", "Wed, 11 May 2017 13:51:18 GMT")
+	w.Header().Set("ETag", "82e-5a6b46e2a93c2")
+
+	if r.Method == "HEAD" {
+		// is this all there is for a HEAD?
+		w.Header().Set("Content-Length", strconv.Itoa(len(robotsTxt)))
+		return
+	}
+	fmt.Fprintf(w, robotsTxt)
+}
