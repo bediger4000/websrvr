ded7573d52baef194c6108b26800ffba44ed4472
Bruce Ediger <bediger8@gmail.com>
2022-05-12 18:01:03 -0600

    HNAP1 method handler, tarpits such requests

--- srvr/head.go
+++ srvr/head.go
@@ -1,6 +1,17 @@
 package srvr
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
+
+func handleHnap(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Last-Modified", "Wed, 11 May 2017 13:51:18 GMT")
+	w.Header().Set("Content-Type", "application/xml")
+	w.Header().Set("Are-You-Kidding", "do/not/be/stupid")
+	time.Sleep(25 * time.Second)
+}
 
 func handleHead(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Last-Modified", "Wed, 11 May 2017 13:51:18 GMT")
